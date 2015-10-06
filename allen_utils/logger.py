12c0b069737d2e98131cb0e0b03b8b639614ec6c
Allen Tran <allen@metromile.com>
2015-10-06 13:12:14 -0700

    return logger

--- allen_utils/logger.py
+++ allen_utils/logger.py
@@ -1,4 +1,15 @@
 import logging
 
-def setup():
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+    return logger
